c4891429f2213465208ad12251dea4ff3329f281
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-05 15:34:44 +0100

    Updated producing the correct data lists

--- moduls/writeList.py
+++ moduls/writeList.py
@@ -1,14 +1,14 @@
 import os
 import shutil
 
+
 def writeListToFile(opts, dataList, outDir, listIdx, singleListOutput=None):
     from stuff import getListPath
-    
+
     if singleListOutput is not None:
         listPath = singleListOutput
-    homeWD = os.getcwd()
-    os.chdir(outDir)
     tmpDirName = outDir + "/" + "job_" + str(listIdx)
+    good_dir = False
     if not os.path.isdir(tmpDirName):
         try:
             os.mkdir(tmpDirName)
@@ -16,6 +16,7 @@ def writeListToFile(opts, dataList, outDir, listIdx, singleListOutput=None):
             print('Creation of the output directory {} failed'.format(tmpDirName))
             raise
         else:
+            good_dir = True
             if opts.verbose:
                 print('Succesfully created output directory: {}'.format(tmpDirName))
     else:
@@ -23,23 +24,28 @@ def writeListToFile(opts, dataList, outDir, listIdx, singleListOutput=None):
             print('Using existing output path directory: {}'.format(tmpDirName))
         if opts.recreate:
             if opts.verbose:
-                print("The directory, containing the following files, will be deleted...")
+                print(
+                    "The directory, containing the following files, will be deleted...")
                 print(os.listdir(tmpDirName))
             shutil.rmtree(tmpDirName)
             os.mkdir(tmpDirName)
-    listPath = getListPath(tmpDirName, listIdx) + ".txt"
-    try:
-        with open(listPath, 'w') as outTmpList:
-            for idx, file in enumerate(dataList):
-                if idx == 0:
-                    outTmpList.write(file)
-                else:
-                    outTmpList.write("\n%s" % file)
-    except OSError:
-        print('Creation of the output data list {} failed'.format(listPath))
-        raise
-    else:
-        if opts.verbose:
-            print('Created output list: {}'.format(listPath))
-    os.chdir(homeWD)
-    return tmpDirName
+            good_dir = True
+        else:
+            good_dir = False
+    if good_dir:
+        listPath = getListPath(tmpDirName, listIdx) + ".txt"
+        try:
+            with open(listPath, 'w') as outTmpList:
+                for idx, file in enumerate(dataList):
+                    if idx == 0:
+                        outTmpList.write(file)
+                    else:
+                        outTmpList.write("\n%s" % file)
+        except OSError:
+            print('Creation of the output data list {} failed'.format(listPath))
+            raise
+        else:
+            if opts.verbose:
+                print('Created output list: {}'.format(listPath))
+
+    return (good_dir, tmpDirName)
