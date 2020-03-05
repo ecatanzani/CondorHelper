635410ef753cea44d4071c4e7e74861540e0636c
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-05 15:43:38 +0100

    Minor bug fixed

--- moduls/HTCSubmit.py
+++ moduls/HTCSubmit.py
@@ -3,5 +3,6 @@ import subprocess
 
 def submitJobs(opts, condorDirs):
     for folder in condorDirs:
+        subFilePath = folder + str("/crawler.sub")
         subprocess.run(
-            ["condor_submit", "-name sn-01.cr.cnaf.infn.it -spool crawler.sub"], shell=True, check=True)
+            ["condor_submit", '-name sn-01.cr.cnaf.infn.it -spool {}'.format(subFilePath)], shell=True, check=True)
