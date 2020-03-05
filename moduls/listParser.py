e0f6fbcc709115d573c386bec249ea73764f21cf
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-05 15:35:49 +0100

    Updated the list parsing
    Now just the correct directories will be used bu the software

--- moduls/listParser.py
+++ moduls/listParser.py
@@ -3,19 +3,26 @@ from writeList import writeListToFile
 
 def parseInputList(opts):
     condorDirs = []
+    condorIdx = []
     outDir = opts.directory
     inputList = open(opts.list, 'r')
     listIdx = 0
     dataList = []
-    for idx, fileName in enumerate(inputList):
+    for fileName in inputList:
         if not fileName.startswith('.'):
             dataList.append(fileName.rstrip('\n'))
             if len(dataList) == opts.fileNumber:
-                condorDirs.append(writeListToFile(opts, dataList, outDir, listIdx))
+                _dir_data = writeListToFile(opts, dataList, outDir, listIdx)
+                if _dir_data[0]:
+                    condorDirs.append(_dir_data[1])
+                    condorIdx.append(listIdx)
+                    listIdx += 1
                 dataList.clear()
-                listIdx += 1
     if dataList:
-        condorDirs.append(writeListToFile(opts, dataList, outDir, listIdx))
+        _dir_data = writeListToFile(opts, dataList, outDir, listIdx)
+        if _dir_data[0]:
+            condorDirs.append(_dir_data[1])
+            condorIdx.append(listIdx)
+            listIdx += 1
         dataList.clear()
-        listIdx += 1
-    return listIdx, condorDirs
+    return listIdx, condorDirs, condorIdx
