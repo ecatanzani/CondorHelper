90605304ad0a37a0efdd7a17a6c6c6b12cdd8e55
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-06 12:46:20 +0100

    Minor bux fix
    Condor has problems with the previus subprocess comand parsing the optional arguments. This new command has been tested

--- moduls/HTCSubmit.py
+++ moduls/HTCSubmit.py
@@ -4,5 +4,4 @@ import subprocess
 def submitJobs(opts, condorDirs):
     for folder in condorDirs:
         subFilePath = folder + str("/crawler.sub")
-        subprocess.run(
-            ["condor_submit", '-name sn-01.cr.cnaf.infn.it -spool {}'.format(subFilePath)], shell=True, check=True)
+        subprocess.run(['condor_submit -name sn-01.cr.cnaf.infn.it -spool {}'.format(subFilePath)], shell=True, check=True)
