9e88e020d2871f66466ad16f9475b9dcf2f654ac
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-04-09 16:08:10 +0200

    Minor bug fix in options parsing

--- condorDeploy.py
+++ condorDeploy.py
@@ -7,17 +7,17 @@ def main(args=None):
     # Parsing options
     parser = ArgumentParser(
         usage="Usage: %(prog)s [options]", description="Condor Deploy Helper")
-    
+
     parser.add_argument("-l", "--list", type=str,
                         dest='list', help='Input file list')
     parser.add_argument("-d", "--dir", type=str,
                         dest='directory', help='Target Directory')
     parser.add_argument("-x", "--executable", type=str,
-                        dest='executable', help='Analysis script')                    
+                        dest='executable', help='Analysis script')
     parser.add_argument("-n", "--number", type=int, dest='fileNumber',
                         const=100, nargs='?', help='number of files per job')
-    parser.add_argument("-i", "--installation", type=str,
-                        dest='installation', help='Load local DAMPE installation')                    
+    parser.add_argument("-i", "--installation", dest='installation',
+                        default=False, action='store_true', help='Load local DAMPE installation')
     parser.add_argument("-v", "--verbose", dest='verbose', default=False,
                         action='store_true', help='run in high verbosity mode')
     parser.add_argument("-r", "--recreate", dest='recreate', default=False,
@@ -30,10 +30,10 @@ def main(args=None):
     from listParser import parseInputList
     from HTCSubmit import submitJobs
     from condorFileWriter import createCondorFiles
-    
+
     if opts.list:
         nDirs, condorDirs, condorIdx = parseInputList(opts)
-    
+
     # Create Condor files
     if opts.installation:
         createCondorFiles(opts, condorDirs, condorIdx, False)
