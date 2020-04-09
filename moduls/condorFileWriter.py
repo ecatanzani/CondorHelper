2f967220569d30a7bb87a2206d2eb522cc96672f
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-04-09 16:53:53 +0200

    Updated local dampesw event source

--- moduls/condorFileWriter.py
+++ moduls/condorFileWriter.py
@@ -47,7 +47,7 @@ def createCondorFiles(opts, condorDirs, condorIdx, completeSoftware=True):
                     outScript.write("#!/usr/bin/env bash\n")
                     outScript.write("source /opt/rh/devtoolset-7/enable\n")
                     outScript.write("source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.36/x86_64-centos7-gcc48-opt/root/bin/thisroot.sh\n")
-                    outScript.write("source /storage/gpfs_data/dampe/users/ecatanzani/Softwares/DAMPE/Event/thisdmpeventclass.sh\n")
+                    outScript.write('cd /storage/gpfs_data/dampe/users/ecatanzani/Softwares/DAMPE/Event && source thisdmpeventclass.sh && cd {}\n'.format(cDir))
                     outScript.write('mkdir {}\n'.format(tmpOutDir))
                     outScript.write('{} -a {} -d {} -v'.format(opts.executable,dataListPath,tmpOutDir))
         except OSError:
