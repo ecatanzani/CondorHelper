5a66aae59d23e256fe590356ace8316c3b8fbbb2
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-01-11 21:03:30 +0100

    Major modification
    Now no while loop is more needed

--- postman/postman.sh
+++ postman/postman.sh
@@ -3,10 +3,16 @@
 _NODE_NAME="sn-01.cr.cnaf.infn.it"
 _CMD="postman.py --config postman.conf"
 _PY="python3"
+_COMPLETED=0
+_SLEEP_TIME=300
 
-NJOBS=`condor_q -name ${_NODE_NAME} ${USER} | grep "Total for query" | awk '{ print $4  }'`
-
-if [ $NJOBS -eq 0 ]
-then
-	$PY $_CMD 
-fi
+while [ $_COMPLETED -eq 0 ]
+do
+	NJOBS=`condor_q -name ${_NODE_NAME} ${USER} | grep "Total for query" | awk '{ print $4  }'`
+	if [ $NJOBS -eq 0 ]
+	then
+		$PY $_CMD
+		let _COMPLETED=1 
+	fi
+	sleep $_SLEEP_TIME
+done
