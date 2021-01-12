80c50bb1911426a54cd25d30e48ae4f85b24a0b6
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-01-12 09:28:27 +0100

    Major bug fix

--- postman/postman.sh
+++ postman/postman.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 _NODE_NAME="sn-01.cr.cnaf.infn.it"
-_CMD="postman.py --config postman.conf"
+_PCMD="postman.py --config postman.conf"
 _PY="python3"
+_CMD="$_PY $_PCMD" 
 _COMPLETED=0
 _SLEEP_TIME=300
 
@@ -11,7 +12,8 @@ do
 	NJOBS=`condor_q -name ${_NODE_NAME} ${USER} | grep "Total for query" | awk '{ print $4  }'`
 	if [ $NJOBS -eq 0 ]
 	then
-		$PY $_CMD
+		echo $_CMD
+		$_CMD
 		let _COMPLETED=1 
 	fi
 	sleep $_SLEEP_TIME
